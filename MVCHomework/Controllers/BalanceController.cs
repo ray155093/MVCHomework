3722dcf95fcf1e7da4922181c6662d22160c8936
ray155093 <ray155093@gmail.com>
2016-08-01 16:38:06 +0800

    完成作業；錯誤:頁面多一個CopyRight，新增資料後回不到Create頁面

---
+++ MVCHomework/Controllers/BalanceController.cs
@@ -0,0 +1,40 @@
+﻿using MVCHomework.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomework.Controllers
+{
+    public class BalanceController : Controller
+    {
+        // GET: Balance
+        public ActionResult Create()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 顯示收支清單
+        /// </summary>
+        /// <returns></returns>
+        [ChildActionOnly]
+        public ActionResult ShowDemoList()
+        {
+            List<AddBalanceViewModel> olists = AddBalanceViewModel.GetDemoList();
+            return View(olists);
+        }
+        /// <summary>
+        /// 新增收支資料
+        /// </summary>
+        /// <param name="oBalance"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddBalance(AddBalanceViewModel oBalance)
+        {
+            AddBalanceViewModel.AddBalance(oBalance);
+            List<AddBalanceViewModel> olists = AddBalanceViewModel.GetDemoList();
+            return View(Create());
+        }
+    }
+}
---
+++ MVCHomework/Models/ViewModels/AddBalanceViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomework.Models.ViewModels
+{
+    public class AddBalanceViewModel
+    {
+        /// <summary>
+        ///  收支類型
+        /// </summary>
+        public enum EBalanceType : int
+        {
+            支出 = 1,
+            收入 = 2
+        }
+        /// <summary>
+        /// 收支類型
+        /// </summary>
+        [DisplayName("收支類型")]
+        public EBalanceType BalanceType { get; set; }
+        /// <summary>
+        /// 紀錄金額
+        /// </summary>
+        [DisplayName("紀錄金額")]
+        public int BalanceMoney { get; set; }
+        /// <summary>
+        /// 紀錄時間
+        /// </summary>
+        [DisplayName("紀錄時間")]
+        public DateTime BalacneDateTime { get; set; }
+        /// <summary>
+        /// 紀錄備註
+        /// </summary>
+        [DisplayName("紀錄備註")]
+        public string BalanceMemo { get; set; }
+        /// <summary>
+        /// 資料物件
+        /// </summary>
+        private static List<AddBalanceViewModel> oLists = new List<AddBalanceViewModel>();
+       
+        /// <summary>
+        /// 建立測試資料
+        /// </summary>
+        public static void CreateDemoList()
+        {
+            AddBalanceViewModel a = new AddBalanceViewModel();
+            a.BalanceType = EBalanceType.支出;
+            a.BalanceMoney = 2000;
+            a.BalacneDateTime = Convert.ToDateTime("2016/7/12");
+            a.BalanceMemo = "買了電動遊戲";
+            oLists.Add(a);
+            AddBalanceViewModel b = new AddBalanceViewModel();
+            b.BalanceType = EBalanceType.收入;
+            b.BalanceMoney = 200;
+            b.BalacneDateTime = Convert.ToDateTime("2016/7/25");
+            b.BalanceMemo = "發票中獎";
+            oLists.Add(b);
+
+        }
+        /// <summary>
+        /// 取得資料
+        /// </summary>
+        /// <returns></returns>
+        public static List<AddBalanceViewModel> GetDemoList()
+        {
+            if(oLists.Count==0)
+            {
+                CreateDemoList();
+            }
+            return oLists;
+        }
+
+        /// <summary>
+        /// 增加資料
+        /// </summary>
+        /// <param name="oBalance"></param>
+        public static void AddBalance(AddBalanceViewModel oBalance)
+        {
+            oLists.Add(oBalance);
+
+        }
+
+    }
+}
